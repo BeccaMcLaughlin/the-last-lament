8600f0624a7be6b49900fff8b30596cea61de242
tris <daelune@gmail.com>
2024-09-01 23:15:23 +0100

    Add spawn points to the scriptable object

--- Assets/Domain/Inventory/ScriptedItem.cs
+++ Assets/Domain/Inventory/ScriptedItem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Create New Ritual Item", order = 1)]
@@ -6,5 +7,7 @@ public class ScriptedItem : ScriptableObject
     public string ItemName = "New Item";
     public Sprite ItemIcon = null;
     public Rigidbody ItemSpawnObject = null;
+    public int PointCost = 1;
     public AudioClip AudioWhenUsed = null;
+    public GameObject AvailableSpawnPoints = null;
 }
