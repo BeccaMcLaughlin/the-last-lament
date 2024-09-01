fda803f2ba3b62caff28eaaa94ae9e260f8a142d
tris <daelune@gmail.com>
2024-09-01 14:06:56 +0100

    Add FPS controller class to handle walking, camera look and sprint

---
+++ Assets/Domain/Player/FPSController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+
+    // TODO: Move these to game settings file
+    public float mouseSensitivity = 100f;
+    public float defaultCameraFOV = 60f;
+
+    private CharacterController controller;
+    private Camera playerCamera;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+    private float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+        Vector3 move = new Vector3(getPlayerSpeed() * movement.x, -2f, getPlayerSpeed() * movement.z);
+        controller.Move(move * Time.deltaTime);
+
+        HandleMouseLook();
+        HandleFOVIfSprinting();
+    }
+
+    bool isSprinting()
+    {
+        // TODO: This will get more complex to add stamina later
+        return Input.GetButton("Sprint");
+    }
+
+    float getPlayerSpeed()
+    {
+        return isSprinting() ? speed * 2f : speed;
+    }
+
+    void HandleMouseLook()
+    {
+        // Get mouse input values for X and Y axes
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Adjust the vertical rotation and clamp it to prevent looking too far up or down
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Apply the rotation to the entire player object, including the camera
+        transform.localRotation = Quaternion.Euler(xRotation, transform.localEulerAngles.y + mouseX, 0f);
+    }
+
+    void HandleFOVIfSprinting()
+    {
+        playerCamera.fieldOfView = isSprinting() ? defaultCameraFOV * 1.1f : defaultCameraFOV;
+    }
+}
