3f3db21889ed7cc55967809547af99261563bde6
tris <daelune@gmail.com>
2024-09-14 22:36:10 +0100

    Remove unused imports and addmethods to Prey

--- Assets/Domain/Characters/Prey.cs
+++ Assets/Domain/Characters/Prey.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class Prey : MonoBehaviour
+abstract class Prey : MonoBehaviour
 {
     public float priority = 1f;
+
+    public bool isCrouching()
+    {
+        return false;
+    }
+
+    public bool isMakingNoise()
+    {
+        return false;
+    }
 }
