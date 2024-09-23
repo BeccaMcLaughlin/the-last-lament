450a25b51c798e39d949e25ae59769b3202ecbec
Becca <daelune@gmail.com>
2024-09-23 14:05:37 +0100

    Feat/options menu (#13)
    * Add input system
    
    * Add the options for system input and a player helper
    
    * Move to input system folder
    
    * Move existing inputs over to new system
    
    * Add options screen and update event syste to new iput system on other screens
    
    * Add rebind button ui piece
    
    * Add rebind for interact
    
    * add rebind reset
    
    * Add other options

---
+++ Assets/Domain/InputSystem/PlayerActions.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerActions
+{
+    private static PlayerActions _instance;
+    public static PlayerActions Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new PlayerActions();
+            return _instance;
+        }
+    }
+
+    private readonly InputActions inputActions;
+    
+    public float MouseSensitivity { get; set; } = 1.0f;
+    
+    public event System.Action OnCrouchPressed;
+    public event System.Action OnCrouchReleased;
+
+    // Private constructor
+    private PlayerActions()
+    {
+        inputActions = new InputActions();
+        
+        inputActions.Gameplay.Crouch.performed += CrouchPerformed;
+        inputActions.Gameplay.Crouch.canceled += CrouchCanceled;
+
+        LoadAllRebinds();
+        
+        inputActions.Enable();
+    }
+    
+    void LoadAllRebinds()
+    {
+        var gameplayMap = inputActions.Gameplay.Get();
+        foreach (var action in gameplayMap.actions)
+        {
+            string rebinds = PlayerPrefs.GetString(action.name + "Rebinds", string.Empty);
+            if (!string.IsNullOrEmpty(rebinds))
+                action.LoadBindingOverridesFromJson(rebinds);
+        }
+    }
+    
+    private void CrouchPerformed(InputAction.CallbackContext context)
+    {
+        OnCrouchPressed?.Invoke();
+    }
+
+    // Callback when crouch is canceled (button released)
+    private void CrouchCanceled(InputAction.CallbackContext context)
+    {
+        OnCrouchReleased?.Invoke();
+    }
+
+    public bool IsSprinting => inputActions.Gameplay.Sprint.IsPressed();
+
+    public bool IsCrouching => inputActions.Gameplay.Crouch.IsPressed();
+
+    public Vector2 LookValue => inputActions.Gameplay.Look.ReadValue<Vector2>() * MouseSensitivity;
+
+    public Vector2 MoveValue => inputActions.Gameplay.Move.ReadValue<Vector2>();
+
+    public bool IsInteracting => inputActions.Gameplay.Interact.IsPressed();
+
+    public bool IsExiting => inputActions.Gameplay.Exit.IsPressed();
+
+    public int ScrollValue => inputActions.Gameplay.Scroll.ReadValue<int>();
+
+    public bool IsDiscarding => inputActions.Gameplay.Discard.IsPressed();
+
+    public void DisableInputs()
+    {
+        inputActions.Disable();
+    }
+
+    public void EnableInputs()
+    {
+        inputActions.Enable();
+    }
+}
---
+++ Assets/Domain/UI/InteractRebindButton.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InteractRebindButton : MonoBehaviour
+{
+    public InputActionReference actionReference;
+    public Button rebindButton;
+    public Button resetButton;
+    public Text bindingDisplayNameText;
+
+    private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+    void Start()
+    {
+        UpdateBindingDisplay();
+        rebindButton.onClick.AddListener(() => StartRebinding());
+        resetButton.onClick.AddListener(() => ResetRebinds());
+    }
+
+    void UpdateBindingDisplay()
+    {
+        if (actionReference != null && actionReference.action != null)
+        {
+            var bindingIndex = actionReference.action.GetBindingIndexForControl(actionReference.action.controls[0]);
+            string displayString = actionReference.action.GetBindingDisplayString(bindingIndex);
+            bindingDisplayNameText.text = displayString;
+        }
+    }
+
+    public void StartRebinding()
+    {
+        if (rebindingOperation != null)
+            rebindingOperation.Cancel();
+
+        bindingDisplayNameText.text = "Press a key...";
+
+        rebindingOperation = actionReference.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Mouse>/position") // Exclude mouse movement
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation => RebindComplete())
+            .Start();
+    }
+
+    private void RebindComplete()
+    {
+        rebindingOperation.Dispose();
+        rebindingOperation = null;
+
+        UpdateBindingDisplay();
+
+        // Save the rebinds
+        SaveRebinds();
+    }
+
+    void SaveRebinds()
+    {
+        var rebinds = actionReference.action.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString(actionReference.action.name + "Rebinds", rebinds);
+    }
+
+    void ResetRebinds()
+    {
+        actionReference.action.RemoveAllBindingOverrides();
+        PlayerPrefs.DeleteKey(actionReference.action.name + "Rebinds");
+        UpdateBindingDisplay();
+    }
+    
+    void LoadRebinds()
+    {
+        var rebinds = PlayerPrefs.GetString(actionReference.action.name + "Rebinds", string.Empty);
+        if (!string.IsNullOrEmpty(rebinds))
+            actionReference.action.LoadBindingOverridesFromJson(rebinds);
+    }
+}
